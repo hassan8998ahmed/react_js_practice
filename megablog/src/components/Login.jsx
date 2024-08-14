8eb715411e2eb0bed51eb2c6924018aa87dfd103
mirzahassanahamd <ahmad8998hassan@gmail.com>
2024-08-14 19:43:06 +0500

    Start of authlayout

--- megablog/src/components/Login.jsx
+++ megablog/src/components/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
     try {
       const session = await authService.login(data);
       if (session) {
-        userData = await authService.getCurrentUser();
+        const userData = await authService.getCurrentUser();
         if (userData) {
           dispatch(AuthLogin(userData));
           navigate("/");
--- megablog/src/components/SignUp.jsx
+++ megablog/src/components/SignUp.jsx
@@ -28,21 +28,61 @@ function SignUp() {
 
   return (
     <div className="flex items-center justify-center">
-        <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
         <div className="mb-2 flex justify-center">
-            <span className="inline-block w-full max-w-[100px]">
-                <Logo width="100%"></Logo>
-            </span>
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%"></Logo>
+          </span>
         </div>
-        <Link to="/login" className="font-medium text-primary transition-all duration-200 hover:underline">
-        Sigm In</Link>
+        <Link
+          to="/login"
+          className="font-medium text-primary transition-all duration-200 hover:underline"
+        >
+          Sign In
+        </Link>
         <p>
-            {error }
-        </p> 
-        </div>
-        
+          {error && <P className="text-red-600 mt-8 text-center">{error}</P>}
+        </p>
+        <form onSubmit={handleSubmit(create)}>
+          <div className="space-y-5">
+            <Input
+              label="Full Name"
+              placeholder="Enter your full name"
+              {...register("name", {
+                required: true,
+              })}
+            />
+            <Input
+              label="Email: "
+              placeholder="Enter Your Email"
+              type="email"
+              {...register("email", {
+                required: true,
+                validate: {
+                  matchPatern: (value) =>
+                    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                    "Email address must be a valid address",
+                },
+              })}
+            />
+            <Input
+              label="password"
+              placeholder="password"
+              type="passowrd"
+              {...register("password", {
+                required: true,
+              })}
+            />
+            <Button type="submit" className="w-full">
+              Create Account
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SignUp;
